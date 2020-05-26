4fff213efcf7b32d62560a371d10d3c3e2652cd3
Hasan Gondal <uh@hasan.wtf>
2020-05-26 03:15:52 +0100

    Add a workerpool to prevent too many goroutines spawning and add more object metadata

--- cmd/monitor/monitor.go
+++ cmd/monitor/monitor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -16,8 +17,8 @@ import (
 
 	"cloud.google.com/go/storage"
 	"github.com/google/uuid"
-
 	"golang.org/x/crypto/sha3"
+
 	"mvdan.cc/xurls/v2"
 )
 
@@ -52,10 +53,22 @@ type Monitor struct {
 	// FileHashes is makeshift Set which contains
 	// all previously detected files.
 	FileHashes map[string]bool
+}
 
-	// StorageClient is a Google Cloud Storage
-	// Client where files will be archived.
-	StorageClient *storage.Client
+// ScanURL is a struct of a URL found by the parser.
+// It contains a depth limit, which prevents scanning
+// too far.
+type ScanURL struct {
+	// URL is the URL of the file as string.
+	URL string
+
+	// GroupTag helps group together files
+	// from Supreme's website.
+	GroupTag string
+
+	// ScanDepth is the depth limit left based
+	// on this ScanURL.
+	ScanDepth uint8
 }
 
 // File is a struct contains parameters
@@ -75,6 +88,11 @@ type File struct {
 	// GroupTag helps group together files
 	// from Supreme's website.
 	GroupTag string
+
+	// ResponseHeader contains a slice of
+	// headers sent from the server, they may
+	// be used in preventing bots so a log is saved.
+	ResponseHeader http.Header
 }
 
 var (
@@ -87,13 +105,13 @@ var (
 	// ErrInvalidStatus occurs when a response's status is not 200 OK
 	ErrInvalidStatus = errors.New("monitor: invalid response status code")
 
-	// URLBlacklist contains a list of URLs to ignore in the monitor
-	URLBlacklist = map[string]bool{
+	// ForbiddenUrls contains a list of URLs to ignore in the monitor
+	ForbiddenUrls = map[string]bool{
 		"google-analytics.com/ga.js": true,
 	}
 
-	// HostBlacklist contains a list of hostnames to ignore in the monitor
-	HostBlacklist = map[string]bool{
+	// ForbiddenHosts contains a list of hostnames to ignore in the monitor
+	ForbiddenHosts = map[string]bool{
 		"cdn.mxpnl.com":                 true,
 		"www.google.com":                true,
 		"songbird.cardinalcommerce.com": true,
@@ -146,12 +164,19 @@ func createMonitor(siteConfig *SiteConfig, webhookURL string, bucket *storage.Bu
 // in the SiteConfig
 func (m *Monitor) Start() {
 	fileChannel := make(chan *File)
+	urlChannel := make(chan *ScanURL)
 
-	go m.ProcessFiles(fileChannel)
+	// Start 20 goroutines to process multiple files concurrently
+	// without leaking goroutines.
+	for i := 0; i < 10; i++ {
+		go m.ProcessFiles(fileChannel)
+		go m.ProcessScanUrls(urlChannel, fileChannel)
+	}
 
 	for {
 		groupTag := uuid.New().String()
 		urls, err := m.Get(m.SiteConfig.URL, fileChannel, groupTag)
+
 		if err != nil {
 			// Handle Error
 			time.Sleep(time.Duration(m.SiteConfig.Delay) * time.Millisecond)
@@ -159,7 +184,11 @@ func (m *Monitor) Start() {
 		}
 
 		for _, url := range urls {
-			go m.ScanURL(url, fileChannel, groupTag, m.SiteConfig.ScanDepth)
+			urlChannel <- &ScanURL{
+				URL:       url,
+				GroupTag:  groupTag,
+				ScanDepth: m.SiteConfig.ScanDepth,
+			}
 		}
 
 		time.Sleep(time.Duration(m.SiteConfig.Delay) * time.Millisecond)
@@ -171,32 +200,25 @@ func (m *Monitor) Start() {
 func (m *Monitor) ProcessFiles(fileChannel <-chan *File) {
 	for file := range fileChannel {
 		if file != nil {
-			go m.HandleFile(file)
-		}
-	}
-}
+			hashBytes := sha3.Sum256(file.Bytes)
+			file.Hash = hex.EncodeToString(hashBytes[:])
+			if _, exists := m.FileHashes[file.Hash]; !exists {
+				m.Mutex.Lock()
+				m.FileHashes[file.Hash] = true
+				m.Mutex.Unlock()
 
-// HandleFile processes a singular file and atempts
-// to uploads if it doesn't already exist.
-func (m *Monitor) HandleFile(file *File) {
-	hashBytes := sha3.Sum256(file.Bytes)
-	file.Hash = hex.EncodeToString(hashBytes[:])
-	if _, exists := m.FileHashes[file.Hash]; !exists {
-		m.Mutex.Lock()
-		m.FileHashes[file.Hash] = true
-		m.Mutex.Unlock()
+				for i := 0; i < 5; i++ {
+					err := m.UploadFile(file)
 
-		for i := 0; i < 5; i++ {
-			err := m.UploadFile(file)
+					if err == nil {
+						return
+					}
+					// Handle Error
+					fmt.Println(err)
+				}
 
-			if err == nil {
-				return
 			}
-
-			// Handle Error
-			fmt.Println(err)
 		}
-
 	}
 }
 
@@ -213,7 +235,7 @@ func (m *Monitor) UploadFile(file *File) error {
 	}
 
 	basePath := path.Base(file.URL.Path)
-	name := fmt.Sprintf("%s/%s/%s_%s", time.Now().Format(time.RFC1123), file.GroupTag, hex.EncodeToString(randomBytes), basePath)
+	name := fmt.Sprintf("%s/%s/%s_%s", time.Now().Format(time.RFC3339), file.GroupTag, hex.EncodeToString(randomBytes), basePath)
 	object := m.Bucket.Object(name)
 
 	w := object.NewWriter(context.Background())
@@ -227,14 +249,26 @@ func (m *Monitor) UploadFile(file *File) error {
 		return err
 	}
 
+	metadata := map[string]string{
+		"x-fm-file-url":   file.URL.String(),
+		"x-fm-shake-256":  file.Hash,
+		"x-fm-timestamp":  time.Now().Format(time.RFC3339Nano),
+		"x-fm-proxy-url":  m.SiteConfig.ProxyURL,
+		"x-fm-user-agent": m.Header.Get("User-Agent"),
+	}
+
+	if len(file.ResponseHeader) > 0 {
+		bytes, err := json.Marshal(&file.ResponseHeader)
+		if err == nil {
+			metadata["x-fm-response-headers"] = string(bytes)
+		} else {
+			metadata["x-fm-response-headers-error"] = err.Error()
+
+		}
+	}
+
 	uattrs := storage.ObjectAttrsToUpdate{
-		Metadata: map[string]string{
-			"file-url":   file.URL.String(),
-			"shake-256":  file.Hash,
-			"timestamp":  time.Now().Format(time.RFC3339Nano),
-			"proxy-url":  m.SiteConfig.ProxyURL,
-			"user-agent": m.Header.Get("User-Agent"),
-		},
+		Metadata: metadata,
 	}
 
 	_, err = object.Update(context.Background(), uattrs)
@@ -242,25 +276,31 @@ func (m *Monitor) UploadFile(file *File) error {
 		return err
 	}
 
-	fmt.Printf("Wrote File - %s_%s - %s\n", hex.EncodeToString(randomBytes), basePath, file.Hash)
+	fmt.Printf("Wrote File - %s\n", object.ObjectName())
 	return nil
 }
 
-// ScanURL goes deeper into each url specified.
-// If scanDepth is more than zero it recurses
-// till scanDepth reaches zero.
-func (m *Monitor) ScanURL(url string, fileChannel chan<- *File, groupTag string, scanDepth uint8) {
-	urls, err := m.Get(url, fileChannel, groupTag)
+// ProcessScanUrls receives URL strings to scan from a channel and attempts to
+// retrieve any URLs found in the file. If there are any URLs and the ScanDepth
+// is more than zero, it will queue more URLs to scan.
+func (m *Monitor) ProcessScanUrls(urlChannel chan *ScanURL, fileChannel chan<- *File) {
+	for scanURL := range urlChannel {
+		urls, err := m.Get(scanURL.URL, fileChannel, scanURL.GroupTag)
 
-	if err != nil {
-		// Handle Error
-		fmt.Println(err)
-		return
-	}
+		if err != nil {
+			// Handle Error
+			fmt.Println(err)
+			continue
+		}
 
-	if scanDepth > 0 {
-		for _, url := range urls {
-			go m.ScanURL(url, fileChannel, groupTag, scanDepth-1)
+		if scanURL.ScanDepth > 0 {
+			for _, url := range urls {
+				urlChannel <- &ScanURL{
+					URL:       url,
+					GroupTag:  scanURL.GroupTag,
+					ScanDepth: scanURL.ScanDepth - 1,
+				}
+			}
 		}
 	}
 }
@@ -303,9 +343,10 @@ func (m *Monitor) Get(rawurl string, fileChannel chan<- *File, groupTag string) 
 		}
 
 		fileChannel <- &File{
-			URL:      resp.Request.URL,
-			Bytes:    body,
-			GroupTag: groupTag,
+			URL:            resp.Request.URL,
+			Bytes:          body,
+			GroupTag:       groupTag,
+			ResponseHeader: resp.Header,
 		}
 
 		bodyString := string(body)
@@ -325,19 +366,18 @@ func (m *Monitor) Get(rawurl string, fileChannel chan<- *File, groupTag string) 
 // provided unfilteredUrls parameter and returns a new slice.
 func filterURLSlice(unfilteredUrls []string) (filteredUrls []string) {
 	for _, rawurl := range unfilteredUrls {
-		if strings.HasSuffix(rawurl, ".js") || strings.HasSuffix(rawurl, ".wasm") {
-			rawurl = strings.TrimSpace(rawurl)
+		if strings.HasSuffix(rawurl, ".js") || strings.HasSuffix(rawurl, ".wasm") || strings.HasSuffix(rawurl, ".css") {
 			URL, err := url.Parse(rawurl)
 
 			if err != nil {
 				continue
 			}
 
-			if _, ok := URLBlacklist[URL.String()]; ok {
+			if _, ok := ForbiddenUrls[URL.String()]; ok {
 				continue
 			}
 
-			if _, ok := HostBlacklist[URL.Hostname()]; ok {
+			if _, ok := ForbiddenHosts[URL.Hostname()]; ok {
 				continue
 			}
 
